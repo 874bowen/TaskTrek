abd6be859e17961ef05613e7682c1fa89951eda9
Ivan Bowen <bowenivan16@gmail.com>
2024-02-12 00:39:22 +0300

    update folder structure with simple register endpoint

--- server/app.py
+++ server/app.py
@@ -1,6 +1,34 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+if 'WEBSITE_HOSTNAME' not in os.environ:
+    # local development, where we'll use environment variables
+    print("Loading config.development and environment variables from .env file.")
+    app.config.from_object('config.development')
+else:
+    # production
+    print("Loading config.production.")
+    app.config.from_object('config.production')
+
+# 127.0.0.1
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+app.config['SECRET_KEY'] = "ade24rset6TEFY4434fdy4ss"
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from flask_cors import CORS
+CORS(app)
+
+from appl.routes.auth import auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route("/")
 def hello_world():
---
+++ server/appl/routes/auth.py
@@ -0,0 +1,27 @@
+# app/routes/auth.py
+
+from flask import Blueprint, request, jsonify
+from appl.models.user import User
+from app import db
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    # Logic to register a new user
+    data = request.get_json()
+    # Create a new user object and add it to the database
+    new_user = User(username=data['username'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    # Logic to authenticate user and generate JWT token
+    pass
+
+@auth_bp.route('/logout', methods=['GET'])
+def logout():
+    # Logic to logout user
+    pass
