d854e41b3d9bf314dba1dc0a0da17bfc4cd58f27
Ivan Bowen <bowenivan16@gmail.com>
2024-02-16 14:29:28 +0300

    update login and register

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,8 +1,41 @@
+import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-import Layout from "./Layout"
+import "react-toastify/dist/ReactToastify.css";
+import Layout from "./Layout";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${import.meta.env.VITE_SERVER_URL}/auth/register`, formData)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.status === "success") {
+          toast.success(res.data.message);
+          navigate("/login");
+        } else {
+          toast.error(res.data.message);
+        }
+      });
+  };
+
   return (
     <Layout>
       <form>
@@ -11,11 +44,14 @@ const Register = () => {
         <div className="form-outline mb-3">
           <input
             type="email"
-            id="form2Example11"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={formData.email}
             className="form-control"
             placeholder="Enter email address"
           />
-          <label className="form-label" htmlFor="form2Example11">
+          <label className="form-label" htmlFor="email">
             Email
           </label>
         </div>
@@ -23,25 +59,42 @@ const Register = () => {
         <div className="form-outline mb-3">
           <input
             type="text"
-            id="form2Example11"
+            id="username"
+            name="username"
+            onChange={handleChange}
+            value={formData.username}
             className="form-control"
             placeholder="Username"
           />
-          <label className="form-label" htmlFor="form2Example11">
+          <label className="form-label" htmlFor="username">
             Username
           </label>
         </div>
 
         <div className="form-outline mb-3">
-          <input type="password" id="form2Example22" className="form-control" />
-          <label className="form-label" htmlFor="form2Example22">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            value={formData.password}
+            className="form-control"
+          />
+          <label className="form-label" htmlFor="password">
             Password
           </label>
         </div>
 
         <div className="form-outline mb-3">
-          <input type="password" id="form2Example22" className="form-control" />
-          <label className="form-label" htmlFor="form2Example22">
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            onChange={handleChange}
+            value={formData.confirmPassword}
+            className="form-control"
+          />
+          <label className="form-label" htmlFor="confirmPassword">
             Confirm Password
           </label>
         </div>
@@ -50,6 +103,7 @@ const Register = () => {
           <button
             className="btn btn-block fa-lg gradient-custom-2"
             type="button"
+            onClick={handleSubmit}
           >
             Create Account
           </button>
@@ -57,13 +111,19 @@ const Register = () => {
 
         <div className="d-flex align-items-center justify-content-center pb-4">
           <p className="mb-0 me-2">Already have an account?</p>
-          <button type="button" className="btn btn-outline-danger" onClick={() => navigate("/login")}>
+          <button
+            type="button"
+            className="btn btn-outline-danger"
+            onClick={() => navigate("/login")}
+          >
             Login
           </button>
         </div>
       </form>
-    </Layout>
-  )
-}
 
-export default Register
+      <ToastContainer />
+    </Layout>
+  );
+};
+
+export default Register;
