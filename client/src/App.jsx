cbf25b4cb754cfca11b3c52c69a6bacd7f543596
Ivan Bowen <bowenivan16@gmail.com>
2024-02-16 14:27:02 +0300

    update routes and add context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,10 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Tasks from "./components/Tasks/Tasks";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PrivateRoutes from './routes/PrivateRoutes';
+import { createContext, useState } from 'react';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Tasks />,
-  },
   {
     path: "/login",
     element: <Login />,
@@ -18,12 +16,40 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
   },
+  {
+    element: <PrivateRoutes />,
+    children: [
+      {
+        path: "/",
+        element: <Tasks />,
+      },
+    ],
+  },
 ]);
 
+export const StatusModalContext = createContext();
+export const AuthContext = createContext();
+
 const App = () => {
+  const [statusData, setStatusData] = useState({
+    status: false,
+    loading: false,
+    message: "",
+    type: "",
+  });
+
+  const [auth, setAuth] = useState({
+    id: null,
+    user: "",
+    token: "",
+  });
   return (
     <div className="" style={{ backgroundColor: "whitesmoke" }}>
-      <RouterProvider router={router} />
+      <StatusModalContext.Provider value={{ statusData, setStatusData }}>
+        <AuthContext.Provider value={{ auth, setAuth }}>
+          <RouterProvider router={router} />
+        </AuthContext.Provider>
+      </StatusModalContext.Provider>
     </div>
   );
 };
